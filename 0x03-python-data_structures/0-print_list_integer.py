29accdd25625909e393d16616a40f228fc858b98
Adeyemi Samuel Adewale <adeyemifrodo@gmail.com>
2022-09-26 16:03:51 -0700

    a function that prints all integers of a list

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,6 @@
+def print_list_integer(my_list=[])
+'''
+    A function that prints all integers of a list
+'''
+    print(my list)
+    return 0
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,2 @@
+
+#A project by ALX on Python - Data Structures: Lists, Tuples
