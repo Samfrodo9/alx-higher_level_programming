28d7ebdc49b20dc1c6be7a737ed8dfe8d934de92
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-14 09:30:42 +0000

    Write a function in C that checks if a singly linked list is a palindrome.

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,44 +1,71 @@
+#include "lists.h"
+#include <stddef.h>
 
 /**
-* is_palindrome -  checks if a singly linked list is a palindrome.
-* @head: pointer to the list
-* Return: 0 or 1
-*/
+ * reverse_listint - Reverses a singly-linked list
+ * @head: A pointer to the starting node
+ *
+ * Return: A pointer to the head
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *nod = *head, *next, *previous = NULL;
+
+	while (nod)
+	{
+		next = nod->next;
+		nod->next = previous;
+		previous = nod;
+		nod = next;
+	}
+
+	*head = previous;
+	return (*head);
+}
+
+/**
+ * is_palindrome - Checks for a palindrome.
+ * @head: A pointer to the head
+ *
+ * Return: If  not palindrome - 0.
+ *         If palindrome - 1.
+ */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp, *temp_;
-	int i, j, k, len, *array = NULL;
+	listint_t *temp, *revy, *middle;
+	size_t size = 0, x;
 
-	if (head == NULL || *head == NULL)
+	if (*head == NULL || (*head)->next == NULL)
 		return (1);
+
 	temp = *head;
-	temp_ = *head;
-	len = 0;
 	while (temp)
 	{
-		len++;
+		size++;
 		temp = temp->next;
 	}
-	array = malloc(sizeof(int) * (len));
-	if (array == NULL)
-		return (-1);
 
-	for (i = 0; temp_; i++)
+	temp = *head;
+	for (x = 0; x < (size / 2) - 1; x++)
+		temp = temp->next;
+
+	if ((size % 2) == 0 && temp->n != temp->next->n)
+		return (0);
+
+	temp = temp->next->next;
+	revy = reverse_listint(&temp);
+	middle = revy;
+
+	temp = *head;
+	while (revy)
 	{
-		array[i] = temp_->n;
-		temp_ = temp_->next;
-	}
-	j = len - 1;
-	for (k = 0; k < (len / 2); k++)
-	{
-		if (array[k] != array[j])
-		{
-			free(array);
+		if (temp->n != revy->n)
 			return (0);
-		}
-		j--;
+		temp = temp->next;
+		revy = revy->next;
 	}
-	free(array);
+	reverse_listint(&middle);
+
 	return (1);
 }
