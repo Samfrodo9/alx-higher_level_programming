0a6ad8fd142cf8d01c92d7155773b0d331fbe580
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-30 18:43:13 +0000

    Holla

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import sys
 
+
 def safe_print_integer_err(value):
     try:
-#        if isinstance(value, int):
         print("{:d}".format(value))
         return True
     except ValueError as e:
