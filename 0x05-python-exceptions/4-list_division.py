05f2671edd433b3eef4f21bd6fd2cc79935d1994
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-08-30 18:01:59 +0000

    list divisions

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -9,7 +9,9 @@ def list_division(my_list_1, my_list_2, list_length):
             newlist.append(0)
         except ZeroDivisionError:
             print("division by 0")
+            newlist.append(0)
         except IndexError:
             print("out of range")
         finally:
             pass
+    return newlist
