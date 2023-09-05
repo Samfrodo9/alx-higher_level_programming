c008bf1a4d2be801207e50975bfeb9c9980292c2
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-05 19:53:26 +0000

    Additional Tests Added

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""A module that prints a Square"""
+
+def print_square(size):
+    """A function that prints a square with character #"""
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for i in range(size):
+        print('#' * size)
