7a242534dde891eb1f155b6df1ab7c7f6e574fc0
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-05 19:20:51 +0000

    Tests Done

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -4,12 +4,13 @@
 
 
 def say_my_name(first_name, last_name=""):
+    """A function that prints a name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     if last_name:
         print(f"My name is {first_name} {last_name}")
     else:
-         print(f"My name is {first_name}")
+        print(f"My name is {first_name}")
