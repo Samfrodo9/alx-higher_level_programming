8e204c6a207e60fde092f01fd61c94112c7db2c5
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-21 22:05:02 +0000

    Added attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from models.base import Base
+from base import Base
 
 """
     A Rectangle Module defined here
@@ -7,24 +7,10 @@ from models.base import Base
 
 
 class Rectangle(Base):
-    """
-    Rectangle A class that defines a Rectangle
-
-    Args:
-        Base (A class): Inherits from the Base Class
-    """
+    """ Rectangle: A class that defines a Rectangle """
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """
-        __init__ initialization of the instance of a rectangle
-
-        Args:
-            width (int): width of rectangle
-            height (int): height of rectangle
-            x (int, optional): x. Defaults to 0.
-            y (int, optional): y. Defaults to 0.
-            id (int, optional): id. Defaults to None.
-        """
+        """ initialization of the instance of a rectangle """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -35,31 +21,18 @@ class Rectangle(Base):
 
         @property
         def width(self):
-            """
-            width getter for private attribute 'width' of rectangle
-
-            Returns:
-                int: width of rectangle
-            """
+            """width getter for private attribute 'width' of rectangle"""
             return self.__width
 
         @width.setter
         def width(self, value):
-            """
-            width setter for private attribute 'width' of rectangle
-
-            Args:
-                value (int): width of rectangle
-            """
+            """width setter for private attribute 'width' of rectangle"""
             self.__width = value
 
         @property
         def height(self):
             """
             height getter for private attribute 'height' of rectangle
-
-            Returns:
-                int: height of rectangle
             """
             return self.__height
 
@@ -67,48 +40,25 @@ class Rectangle(Base):
         def height(self, value):
             """
             height setter for private attribute 'width' of rectangle
-
-            Args:
-                value (int): height of rectangle
             """
             self.__height = value
 
         @property
         def x(self):
-            """
-            x getter for private attribute 'x' of rectangle
-
-            Returns:
-                int: x of rectangle
-            """
+            """x getter for private attribute 'x' of rectangle"""
             return self.__x
 
         @x.setter
         def x(self, value):
-            """
-            x setter for private attribute 'width' of rectangle
-
-            Args:
-                value (int): x of rectangle
-            """
+            """x setter for private attribute 'width' of rectangle"""
             self.__x = value
 
         @property
         def y(self):
-            """
-            y getter for private attribute 'y' of rectangle
-
-            Returns:
-                int: y of rectangle
-            """
+            """ y getter for private attribute 'y' of rectangle"""
             return self.__y
 
         @y.setter
         def y(self, value):
-            """
-            y setter for private attribute 'width' of rectangle
-
-            Args:
-                value (int): y of rectangle
-            """
+            """y setter for private attribute 'width' of rectangle"""
             self.__y = value
