dd93014b6a86760ffdf6df290a1b0376aebbcb2a
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-21 23:11:21 +0000

    Added str magic method

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -112,3 +112,9 @@ class Rectangle(Base):
         """
         for i in range(self.__height):
             print('#' * self.__width)
+
+    def __str__(self):
+        """
+        __str__ a method to a string representation of rectangle
+        """
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
