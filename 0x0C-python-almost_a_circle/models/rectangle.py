c4bce79374aeac0155a2541142a65e6d64768c44
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-21 23:46:04 +0000

    Modified display method with x and y included

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -111,9 +111,9 @@ class Rectangle(Base):
         display displays a symbol '#'
         """
         for i in range(self.__y):
-            print(' ' * self.__x)
+            print()
         for i in range(self.__height):
-                print(' ' * self.__x, '#' * self.__width)
+                print(' ' * self.__x + '#' * self.__width)
 
     def __str__(self):
         """
