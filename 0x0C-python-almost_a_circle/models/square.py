a9bed17c8e636b62ad20ac4293c21f2eea30512d
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 18:24:51 +0000

    Added more error checking

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,38 +1,32 @@
 #!/usr/bin/python3
-'''square module'''
-Rectangle = __import__('models.rectangle').rectangle.Rectangle
+"""The square model, let's write"""
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    '''a square class inherits from rectangle'''
-    def __init__(self, size, x=0, y=0, id=None):
-        '''constructor method'''
-        if type(size) is not int:
-            raise TypeError("width must be an integer")
-        if size <= 0:
-            raise ValueError("width must be > 0")
-        self.__size = size
-        super().__init__(size, size, x, y, id)
+    """A Square module that inherits from a Rectangle"""
 
-    def __str__(self):
-        '''private instance method'''
-        rec = "[Square] ({}) {:d}/{:d} - {:d}"
-        return (rec.format(self.id, self.x, self.y, self.size))
+    def __init__(self, size, x=0, y=0, id=None):
+        '''A constructor method'''
+        super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        '''size getter'''
-        return (self.width)
+        """Size getter for Square."""
+        return self.width
 
     @size.setter
     def size(self, value):
-        '''size setter'''
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Size setter for Square."""
         self.width = value
         self.height = value
-        self.__size = value
 
-#    def update(self, *args,)
+    def __str__(self):
+        """
+        __str__ A string rep of Square
+
+        Returns:
+                _type_: str
+        """
+        sqr = "[Square] ({}) {:d}/{:d} - {:d}"
+        return sqr.format(self.id, self.x, self.y, self.size)
