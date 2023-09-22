4992561a6437e61ef103d026e4c531a775b372d3
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 16:38:30 +0000

    Added getter and setter for Width

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,3 +22,17 @@ class Square(Rectangle):
         """
         sqr = "[Square] ({}) {:d}/{:d} - {:d}"
         return (sqr.format(self.id, self.x, self.y, self.size))
+
+    @property
+    def size(self):
+        """width getter for private attribute 'width' of rectangle"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """width setter for private attribute 'width' of rectangle"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__size = value
