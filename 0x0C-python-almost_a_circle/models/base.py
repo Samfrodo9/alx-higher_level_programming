eb92682bd5ac433e1ecf55e7fac06df43d82b28d
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 19:57:57 +0000

    Added string rep for Base Class

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -24,8 +24,7 @@ class Base:
     @staticmethod
     def to_json_string(list_dictionaries):
         '''A method that returns JSON rep of a list_dictionaries'''
-        if list_dictionaries == None or len(list_dictionaries) == 0:
+        if list_dictionaries is None or len(list_dictionaries) == 0:
             return '[]'
         else:
             return dumps(list_dictionaries)
-
