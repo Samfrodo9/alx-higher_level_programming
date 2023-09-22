9c3cf798dbcf66b216724f1774b66f8b8fff5285
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 14:32:18 +0000

    Added update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -121,3 +121,26 @@ class Rectangle(Base):
         """
         rec = "[Rectangle] ({}) {:d}/{:d} - {:d}/{:d}"
         return (rec.format(self.id, self.x, self.y, self.width, self.height))
+    
+    def update(self, *args):
+        # Check the length of the args tuple
+        if len(args) == 1:
+            self.id = args[0]
+        if len(args) == 2:
+            self.id = args[0]
+            self.__width = args[1]
+        if len(args) == 3:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+        if len(args) == 4:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+        if len(args) == 5:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
