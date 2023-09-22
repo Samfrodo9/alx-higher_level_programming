f89f53d3c7ed283c72c759d9c1aac7bcbdefd834
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 18:40:53 +0000

    Updated Square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -30,3 +30,21 @@ class Square(Rectangle):
         """
         sqr = "[Square] ({}) {:d}/{:d} - {:d}"
         return sqr.format(self.id, self.x, self.y, self.size)
+    
+    def update(self, *args, **kwargs):
+        """A method that updates the attributes"""
+        if args and len(args) != 0:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                if key in ('id', 'size', 'x', 'y'):
+                    setattr(self, key, value)
+
+            
