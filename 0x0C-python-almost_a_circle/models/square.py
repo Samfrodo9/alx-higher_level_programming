329fc6efe8769f76cbe16422602fc3fa514aa20d
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-09-22 18:02:38 +0000

    Added more error checking

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -22,7 +22,7 @@ class Square(Rectangle):
     @property
     def size(self):
         '''size getter'''
-        return (self.__size)
+        return (self.__width)
 
     @size.setter
     def size(self, value):
