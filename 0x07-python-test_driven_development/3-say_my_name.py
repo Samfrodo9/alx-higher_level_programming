1609cbe5f303ab137966877e251cf7a05cff6177
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-03 21:38:23 +0000

    Normalize whitespace

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -3,14 +3,14 @@
 """ A module that prints a name """
 
 
-def say_my_name(first_name, last_name=" "):
+def say_my_name(first_name, last_name=""):
     """A function that prints a name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
-    if last_name:
-        print(f"My name is {first_name} {last_name}")
-    else:
-        print(f"My name is {first_name} ")
+    #if last_name:
+    print(f"My name is {first_name} {last_name}")
+    #else:
+    #    print(f"My name is {first_name}")
