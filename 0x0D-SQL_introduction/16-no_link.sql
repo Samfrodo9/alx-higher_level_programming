7620b1b21a14d5231966367a08b3df23b6e91c04
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-20 21:45:38 +0000

    Listing name

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 --  a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
 SELECT sore, name FROM second_table
-WHERE name != NULL
+WHERE name IS NOT NULL
 ORDER BY score DESC
