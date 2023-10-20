5bd297ca7da79db1117b15fad225729cf2520ad5
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-20 01:09:47 +0000

    Alter Table

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,4 +1,4 @@
 --  a script that inserts a new row in the table first_table (database hbtn_0c_0) in your MySQL server.
-ALTER first_table(
+ALTER TABLE first_table(
 	id = 89,
 	name = "Best School");
