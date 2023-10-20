3b3a53ad954a5a7c04cd7f6822376bfcb0119073
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-10-20 21:04:04 +0000

    Score set

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,2 +1,2 @@
 -- a script that updates the score of Bob to 10 in the table second_table
-SELECT score, name FROM second_table WHERE name = "Bob" SET score = 10;
+SELECT score, name FROM second_table SET score = 10 WHERE name = "Bob";
