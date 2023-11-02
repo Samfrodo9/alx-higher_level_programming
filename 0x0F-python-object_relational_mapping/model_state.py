e21861cfad856c258db95d24100b854bbf56abe6
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-11-02 21:07:28 +0000

    Done

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-	Creating a state Model
+    Creating a state Model
 """
 
 
@@ -9,9 +9,12 @@ from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
 Base = declarative_base()
-class State(Base):
-	"""A state class"""
-	__tablename__ = "states"
 
-	id =  Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
-	name = Column(String(128), nullable=False)
+
+class State(Base):
+    """A state class"""
+    __tablename__ = "states"
+
+    id = Column(Integer, primary_key=True, autoincrement=True,
+                nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
