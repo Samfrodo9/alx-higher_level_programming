481c422bb6fb1b80033382fb41b4375faa0a170e
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-11-02 16:33:45 +0000

    Pushed

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A script that lists all states from the database hbtn_0e_0_usa """
+
+
+import MYSQLdb
+from sys import argv
+
+
+def run():
+	'''defines the main function'''
+	db_usr = argv[1]
+	db_pass = argv[2]
+	db_name = argv[3]
+	db_host = "localhost"
+	db_port = 3306
+	try:
+		connect = MySQLdb.connect(host=db_host, port=db_port, user=db_usr,
+								passwd=db_pass, db=db_name, charset="utf8")
+		cur = connect.cursor()
+		cur.execute("SELECT * FROM states ORDER BY id ASC")
+		result = cur.fetchall()
+		for i in result:
+			print(i)
+		cur.close()
+		connect.close()
+	except Exception:
+		pass
+
+if __name__ == "main":
+	run()
