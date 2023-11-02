ae7ed020d0121bfc884559130cfa0093b18555fe
Adeyemi Samuel Adewale <Adeyemifrodo@gmail.com>
2023-11-02 16:46:51 +0000

    Sam

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,24 +7,25 @@ from sys import argv
 
 
 def run():
-	'''defines the main function'''
-	db_usr = argv[1]
-	db_pass = argv[2]
-	db_name = argv[3]
-	db_host = "localhost"
-	db_port = 3306
-	try:
-		connect = MySQLdb.connect(host=db_host, port=db_port, user=db_usr,
-								passwd=db_pass, db=db_name, charset="utf8")
-		cur = connect.cursor()
-		cur.execute("SELECT * FROM states ORDER BY id ASC")
-		result = cur.fetchall()
-		for i in result:
-			print(i)
-		cur.close()
-		connect.close()
-	except Exception:
-		pass
+    '''defines the main function'''
+    db_usr = argv[1]
+    db_pass = argv[2]
+    db_name = argv[3]
+    db_host = "localhost"
+    db_port = 3306
+    try:
+        connect = MySQLdb.connect(host=db_host, port=db_port, user=db_usr,
+                                  passwd=db_pass, db=db_name, charset="utf8")
+        cur = connect.cursor()
+        cur.execute("SELECT * FROM states ORDER BY id ASC")
+        result = cur.fetchall()
+        for i in result:
+            print(i)
+        cur.close()
+        connect.close()
+    except Exception:
+        pass
+
 
 if __name__ == "main":
-	run()
+    run()
