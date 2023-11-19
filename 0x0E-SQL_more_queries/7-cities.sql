f76387b413f3b7e8b3c66f0e546ae7adc9ea9b1d
Adeyemi Samuel <adeyemifrodo@gmail.com>
2023-11-19 09:28:40 -0800

    FDoreign Key

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,8 +1,20 @@
-Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
+-- Create the database if it doesn't exist
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
-cities description:
-id INT unique, auto generated, can’t be null and is a primary key
-state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
-name VARCHAR(256) can’t be null
-If the database hbtn_0d_usa already exists, your script should not fail
-If the table cities already exists, your script should not fail
+-- Use the database
+USE hbtn_0d_usa;
+
+-- Create the states table if it doesn't exist
+CREATE TABLE IF NOT EXISTS states (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL
+);
+
+-- Create the cities table if it doesn't exist
+CREATE TABLE IF NOT EXISTS cities (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
+);
+
